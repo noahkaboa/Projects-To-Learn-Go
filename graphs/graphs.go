73eb2ca4c13a2a47c0fea722d3adf9a1e07e95d6
noahkaboa <nphinger@gmail.com>
2025-01-15 15:24:15 -0500

    fixed pointer issue

--- graphs/graphs.go
+++ graphs/graphs.go
@@ -5,24 +5,24 @@ type Node[T any] struct {
 }
 
 type Edge[T any] struct {
-	NodeA *Node[T]
-	NodeB *Node[T]
+	NodeAPtr *Node[T]
+	NodeBPtr *Node[T]
 }
 
 type Graph[T any] struct {
-	nodes []Node[T]
-	edges []Edge[T]
+	nodes []*Node[T]
+	edges []*Edge[T]
 }
 
-func (g *Graph[T]) addNode(value T) Node[T] {
-	newNode := Node[T]{value: value}
+func (g *Graph[T]) addNode(value T) *Node[T] {
+	newNode := &Node[T]{value: value}
 	g.nodes = append(g.nodes, newNode)
 	return newNode
 }
 
-func (g *Graph[T]) addEdgeWithNodes(nodeA Node[T], nodeB Node[T]) {
-	var newEdge Edge[T]
-	newEdge.NodeA = &nodeA
-	newEdge.NodeB = &nodeB
+func (g *Graph[T]) addEdgeWithNodes(nodeA *Node[T], nodeB *Node[T]) {
+	newEdge := &Edge[T]{}
+	newEdge.NodeAPtr = nodeA
+	newEdge.NodeBPtr = nodeB
 	g.edges = append(g.edges, newEdge)
 }
--- graphs/main.go
+++ graphs/main.go
@@ -9,12 +9,16 @@ func main() {
 
 	printGraph(g)
 
-	nodeA := g.addNode("A!")
-	nodeB := g.addNode("B!!")
+	nodeAPtr := g.addNode("A!")
+	nodeBPtr := g.addNode("B!!")
 
 	printGraph(g)
 
-	g.addEdgeWithNodes(nodeA, nodeB)
+	g.addEdgeWithNodes(nodeAPtr, nodeBPtr)
+
+	printGraph(g)
+
+	g.nodes[0].value = "Not A!"
 
 	printGraph(g)
 }
@@ -22,11 +26,14 @@ func main() {
 func printGraph[T any](g Graph[T]) {
 	fmt.Println("Nodes:")
 	for i := 0; i < len(g.nodes); i++ {
-		fmt.Println(&g.nodes[i])
+		fmt.Println(g.nodes[i])
 	}
 	fmt.Println("Edges:")
 	for i := 0; i < len(g.edges); i++ {
-		fmt.Println(&g.edges[i])
+		fmt.Print("NodeA:\t")
+		fmt.Println(*g.edges[i].NodeAPtr)
+		fmt.Print("NodeB:\t")
+		fmt.Println(*g.edges[i].NodeBPtr)
 	}
 
 }
