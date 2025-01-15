5f8d44a55a27884b024af97abd764b2e0aab2b28
noahkaboa <nphinger@gmail.com>
2025-01-15 15:28:43 -0500

    Supports multiple nodes

--- graphs/main.go
+++ graphs/main.go
@@ -21,9 +21,22 @@ func main() {
 	g.nodes[0].value = "Not A!"
 
 	printGraph(g)
+
+	nodeCPtr := g.addNode("its c")
+	nodeDPtr := g.addNode("DDD")
+
+	printGraph(g)
+
+	g.addEdgeWithNodes(nodeCPtr, nodeDPtr)
+
+	printGraph(g)
+
+	g.addEdgeWithNodes(nodeAPtr, nodeCPtr)
+	printGraph(g)
 }
 
 func printGraph[T any](g Graph[T]) {
+	fmt.Println("Printing graph!")
 	fmt.Println("Nodes:")
 	for i := 0; i < len(g.nodes); i++ {
 		fmt.Println(g.nodes[i])
@@ -31,8 +44,8 @@ func printGraph[T any](g Graph[T]) {
 	fmt.Println("Edges:")
 	for i := 0; i < len(g.edges); i++ {
 		fmt.Print("NodeA:\t")
-		fmt.Println(*g.edges[i].NodeAPtr)
-		fmt.Print("NodeB:\t")
+		fmt.Print(*g.edges[i].NodeAPtr)
+		fmt.Print("\t\tNodeB:\t")
 		fmt.Println(*g.edges[i].NodeBPtr)
 	}
 
