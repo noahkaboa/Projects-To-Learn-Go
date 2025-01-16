4ad6a97256879d186596a1d0c3d21e0b9ffb5de0
noahkaboa <nphinger@gmail.com>
2025-01-16 18:13:13 -0500

    Streaming implemented

---
+++ web_scraper/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	targetURL := "https://google.com"
+
+	resp, err := http.Get(targetURL)
+	if err != nil {
+		fmt.Println("Trouble sending get request")
+		fmt.Println(err)
+	}
+
+	fmt.Println(resp.Header)
+
+	bodyBytes := streamBody(resp.Body)
+
+	defer resp.Body.Close()
+
+	fmt.Println(bodyBytes)
+}
+
+func streamBody(rc io.ReadCloser) []byte {
+	bodyBytes := make([]byte, 0)
+	for {
+		streamedByte := make([]byte, 1)
+		count, err := rc.Read(streamedByte)
+		if err != nil {
+			fmt.Println("Error with streaming body")
+		}
+		if count == 0 {
+			return bodyBytes
+		}
+		bodyBytes = append(bodyBytes, streamedByte...)
+	}
+}
